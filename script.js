d560965dc9f6a8e9d64f92bc62933d8fe2032210
rtx0854 <emiliojosuecurevega@gmail.com>
2025-10-04 11:25:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const libros = [
   { 
     titulo: "Antígona González", 
     autor: "Sara Uribe", 
-    portada: "img/Antigona Gonzales.jpg", 
+    portada: "img/antigona gonzales.jpg", 
     descripcion: "Antígona González busca a su hermano desaparecido en Tamaulipas, México, en medio de la violencia del narcotráfico.",
     link: "libros/Antigona Gonzales.pdf",
     genero: "Drama"
@@ -378,3 +378,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
